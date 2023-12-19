8d02d7ae73db83ffca82512482f226c4a5691818
Abhishek Singh <as7686854@gmail.com>
2023-12-19 12:14:04 +0530

    Some Changes

--- src/App.js
+++ src/App.js
@@ -2,35 +2,60 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
-import {Route,Link,RouterProvider,createBrowserRouter} from "react-router-dom"
 import SignupPage from './pages/SignupPage';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from 'react-router-dom';
+import CartPage from './pages/CartPage';
+import ProductDetailPage from './pages/ProductDetailPage';
+import Checkout from './pages/Checkout';
+import Protected from './features/auth/components/Protected';
+
+// changes in Login ,signup,Cart,CheckoutPage
+
+// command to run json server->  json-server --watch data.json --port 8000
 
 const router = createBrowserRouter([
   {
-    path:'/',
-    element:(<Home/>),
+    path: '/',
+    element:
+    // <Protected> 
+    <Home></Home>
+    //  </Protected>,
   },
   {
-    path:'/login',
-    element:(<LoginPage></LoginPage>),
+    path: '/login',
+    element:<LoginPage></LoginPage>,
   },
   {
-    path:'/signup',
-    element:(<SignupPage></SignupPage>),
+    path: '/signup',
+    element: <SignupPage></SignupPage>,
   },
+  { 
+    path: '/cart',
+    element: <Protected><CartPage></CartPage></Protected>,
+  },
+  { 
+    path: '/checkout',
+    element: <Protected><Checkout></Checkout></Protected>,
+  },
+  { 
+    path: '/product-detail/:id',
+    element: 
+    // <Protected>
+    <ProductDetailPage></ProductDetailPage>
+    // </Protected>,
+  },
+]);
 
-])
-
-
-
-function App() {
+export default function App() {
   return (
     <div className="App">
-      {/* <Home></Home>
-      <LoginPage/> */}
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </div>
   );
 }
-
-export default App;
